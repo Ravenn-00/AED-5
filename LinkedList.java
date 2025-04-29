77442c65fdf89019a61c14f8598afd1f54b544af
GRB <freze000@gmail.com>
2025-04-29 16:09:06 -0500

    2

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,64 @@
 public class LinkedList<T> {
     Node<T> head;
     int size;
-    Node<T> aux;    
+    Node<T> aux;
+    public LinkedList() {
+        this.head = null;
+        this.size = 0;
+        this.aux = null;
+    }
+    public LinkedList(Node<T> _head) {
+        this.head = _head;
+        this.size = 1;
+        this.aux = _head;
+    }
+    public boolean isEmpty()    {
+        return this.head == null;
+    }
+    public int lenght() {
+        while (this.aux.getNext() != null) {
+            size += 1;
+            this.aux = this.aux.getNext();
+        }
+        this.aux = this.head;
+        return size;
+    }
+    public void insertFirst(Node<T> _node)  {
+        _node.setNext(this.head);
+        this.head = _node;
+        this.aux = _node;
+        this.size += 1; 
+    }
+    public void insertLast(Node<T> _node)  {
+        while(this.aux.getNext() != null)   {
+            this.aux = this.aux.getNext();
+        }
+        _node.setNext(null);
+        this.aux.getNext().setNext(_node);
+        this.aux = this.head;
+    }
+    public void destroy()   {
+        this.head = null;
+        this.aux = null;
+        this.size = 0;
+    }
+    public boolean search(Node<T> _node)    {
+
+        return true;
+    }
+    public void main(String args[]) {
+        Node<Integer> N1 = new Node<Integer>();
+        Node<Integer> N2 = new Node<Integer>();
+        Node<Integer> N3 = new Node<Integer>();
+        Node<Integer> N4 = new Node<Integer>();
+        Node<Integer> N5 = new Node<Integer>();
+
+        N1.setNext(N2);
+        N2.setNext(N3);
+        N3.setNext(N4);
+        N4.setNext(N5);
+
+        LinkedList<Integer> Lista = new LinkedList<Integer>(N1);
+        System.out.println(Lista.lenght());
+    }
 }
--- Node.java
+++ Node.java
@@ -1,23 +1,28 @@
 public class Node<T> {
-    T data;
-    Node<T> next;
+    private T data;
+    private Node<T> next;
+    public Node()   {
+        this.data = null;
+        this.next = null;
+    }
     public Node(T _data)    {
         this.data = _data;
+        this.next = null;
     }
     public Node(T _data, Node<T> _node)    {
         this.data = _data;
         this.next = _node;
     }
-    public Node<T> getNode()    {
+    public Node<T> getNext()    {
         return this.next;
     }
-    public void setNode(Node<T> _node)    {
+    public void setNext(Node<T> _node)    {
         this.next = _node;
     }
     public T getData()    {
         return this.data;
     }
-    public void setNode(T _data)    {
+    public void setData(T _data)    {
         this.data = _data;
     }
 }
